962bd583394415a81c238e55bf8cf2655e9c7e56
Ignacio <ignacio.habiague1@gmail.com>
2025-03-13 11:18:18 -0300

    gain calculation and choosing best day option

--- stock_picker.rb
+++ stock_picker.rb
@@ -5,7 +5,13 @@ def stock_picker(prices)
     prices.each_with_index do |buy_price, buy_day|
 
     prices[buy_day+1..-1].each_with_index do |sell_price, sell_index|
-
+        
+    profit = sell_price - buy_price
+   
+    if profit > max_profit
+        max_profit = profit
+        best_days = [buy_day, buy_day + 1 + sell_index]
+    end
 end
 
   
